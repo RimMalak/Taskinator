dae476b8f8095e42c8748be94b02de9786667d87
Perlatum <rimmalak@hotmail.com>
2020-12-23 03:45:52 -0500

    event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() 
-{
-    debugger;
+var createTaskHandler = function(event) 
+{   
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className="task-item";
     listItemEl.textContent="This is a new task";
@@ -11,4 +12,4 @@ var createTaskHandler = function()
 };
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
