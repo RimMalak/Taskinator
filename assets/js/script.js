63c00ecb63ab53176bc6b45f05cffa983e1a71e9
Perlatum <rimmalak@hotmail.com>
2020-12-31 02:19:55 -0500

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,6 @@ var taskFormHandler = function(event)
             type: taskTypeInput,
             status: "to do"
         };
-
         createTaskEl(taskDataObj);
     }
 };
@@ -46,25 +45,40 @@ var createTaskEl = function(taskDataObj)
     var listItemEl = document.createElement("li");
     listItemEl.className="task-item";
     listItemEl.setAttribute("data-task-id", taskIdCounter);
-
     listItemEl.setAttribute("draggable", "true");
 
     var taskInfoEl = document.createElement("div");
-    taskInfoEl.className="task-info";
-
+    taskInfoEl.className="task-info"; 
     taskInfoEl.innerHTML="<h3 class='task-name'>" +  taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
-    taskToDoEl.appendChild(listItemEl);
+    
+    switch (taskDataObj.status)
+    {
+        case "to do":
+            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            taskToDoEl.appendChild(listItemEl);
+        break;
+        case "in progress":
+            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        break;
+        case "completed":
+            taskActionsEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        break;
+        default:
+            console.log("something went wrong");
+    }
 
     taskDataObj.id = taskIdCounter;
     tasks.push(taskDataObj);
 
-    taskIdCounter++;
-
     saveTasks();
+
+    taskIdCounter++;
 };
 
 var createTaskActions = function(taskId)
@@ -270,58 +284,20 @@ var saveTasks = function()
 
 var loadTasks = function()
 {
-    tasks = localStorage.getItem("tasks")
-    console.log(tasks);
-    if(tasks === null)
+    var savedTasks = localStorage.getItem("tasks");
+
+    if(!savedTasks)
     {
-        tasks = [];
         return false;
     }
-    tasks = JSON.parse(tasks);
-    console.log(tasks);
 
-    for(var i = 0; i < tasks.length; i++)
+    savedTasks = JSON.parse(savedTasks);
+
+    for(var i = 0; i < savedTasks.length ; i++)
     {
-        tasks[i].id = taskIdCounter;
-
-        var listItemEl = document.createElement("li");
-        listItemEl.className="task-item";
-        listItemEl.setAttribute("data-task-id", tasks[i].id);
-        listItemEl.setAttribute("draggable", "true");
-        console.log(tasks[i]);
-        console.log(listItemEl)
-
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-
-        listItemEl.appendChild(taskInfoEl);
-
-        var taskActionsEl = createTaskActions(tasks[i].id)
-
-        listItemEl.appendChild(taskActionsEl);
-        console.log(listItemEl);
-
-        if(tasks[i].status === "to do")
-        {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0 ;
-            taskToDoEl.appendChild(listItemEl);
-        }
-        else if(tasks[i].status === "in progress")
-        {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1 ;
-            tasksInProgressEl.appendChild(listItemEl);
-        }
-        else if(tasks[i].status === "completed")
-        {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2 ;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-        taskIdCounter++;
+        createTaskEl(savedTasks[i]); 
     }
 };
-
-loadTasks();
 
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -336,3 +312,5 @@ pageContentEl.addEventListener("dragover", dorpZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
+
+loadTasks();
